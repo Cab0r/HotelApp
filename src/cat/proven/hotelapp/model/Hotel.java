84b418b3c57f93d285319e836407f53bf73a28ae
alumne <mario.barroso.ribes@gmail.com>
2021-11-18 14:33:53 +0100

    esqueleto

--- src/cat/proven/hotelapp/model/Hotel.java
+++ src/cat/proven/hotelapp/model/Hotel.java
@@ -1,9 +1,13 @@
 package cat.proven.hotelapp.model;
 
+import java.util.List;
+import java.util.Map;
+
 /**
  *
  * @author Mario Barroso
  */
 public class Hotel {
+    private Map<Room, List<Customer>> hotel;
     
 }
